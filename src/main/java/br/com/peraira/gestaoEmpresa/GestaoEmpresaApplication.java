48ac15139dc85e2073c40a8e89a4bd877ea497b2
wandersonpinto <wanderson.p.pinto@gmail.com>
2022-01-23 11:12:12 -0300

    Commit inicial

---
+++ src/main/java/br/com/peraira/gestaoEmpresa/GestaoEmpresaApplication.java
@@ -0,0 +1,13 @@
+package br.com.peraira.gestaoEmpresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEmpresaApplication.class, args);
+	}
+
+}
